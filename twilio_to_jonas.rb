5fbfbd4dc171ef1ee4561a01fc06a3b94346347f
Henry van Wagenberg <henryvw@gmail.com>
2015-09-03 12:09:45 +0200

    small typo correction

--- twilio_to_jonas.rb
+++ twilio_to_jonas.rb
@@ -3,22 +3,21 @@ require 'bundler'
 require 'dotenv'
 require 'sinatra'
 require 'twilio-ruby'
+Bundler.require
+
+Dotenv.load
 
 Class TwilioJonas < Sinatra::Base
 
-  Bundler.require
-
-  Dotenv.load
-
   account_sid = 'AC05a4b296092d722f091550c84015ab1a'
   auth_token = ENV['AUTH_TOKEN']
-  jonas_cell = ENV['RECEPIENT_CELL']
+  jonas_cell = ENV['RECIPIENT_CELL']
   twilio_phone = ENV['TWILIO_PHONE'] 
 
   first_message = "Hey Jonas, have you set out a glass of water for tomorrow morning?"
   second_message = "Jonas - water!"
 
-  @twilio_client = Twilio::REST::Client.new account_sid, auth_token
+  @twilio_client = Twilio::REST::Client.new(account_sid, auth_token)
 
   message = @twilio_client.account.messages.create(:body => first_message,
                                                  :to => jonas_cell,
@@ -29,4 +28,3 @@ Class TwilioJonas < Sinatra::Base
     'Hello Twilio!' + Twilio::VERSION + \
   end
 end
-
